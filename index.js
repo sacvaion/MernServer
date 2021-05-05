02f5337c08e9b45f3cf2d298de10a93008e63d8b
Carlos.Siabato <sacvaion@gmail.com>
2021-05-05 18:50:33 -0500

    ajustes para deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //Puerto de la APP
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -30,6 +30,6 @@ app.get("/", (req, res) => {
 });
 
 //Arranca la app
-app.listen(PORT, () => {
-  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port,'0.0.0.0', () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
